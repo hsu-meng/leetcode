79b9f88a82474bfc1b11579afa71cbb126da38a0
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-11 16:23:59 +0800

    Add files via upload

--- Array/495_TeemoAttacking/attacking.py
+++ Array/495_TeemoAttacking/attacking.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        if not timeSeries:
+            return 0
+        
+        dura = 0
+        lastTime = 0
+        for i, t in enumerate(timeSeries):
+            if i < len(timeSeries)-1:
+                dura = timeSeries[i+1] - t
+            else:
+                dura = duration
+            if dura > duration:
+                lastTime += duration
+            else:
+                lastTime += dura
+        return lastTime
