8927ee0471f22c5c19c3279916520087096b8fcc
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-11 16:40:40 +0800

    Add files via upload

--- Array/645_SetMismatch/misMatch.py
+++ Array/645_SetMismatch/misMatch.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        lost = 0
+        for i in range(1, len(nums)+1):
+            if i in nums:
+                nums.remove(i)
+            else:
+                lost = i
+                nums.append(lost)
+        return nums
