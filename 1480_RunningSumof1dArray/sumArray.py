04903a10d56b27828c70ff77956d942a3238a047
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-11 16:53:06 +0800

    Delete 1480_RunningSumof1dArray directory

--- 1480_RunningSumof1dArray/sumArray.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        total = []
-        for i, n in enumerate(nums):
-            if i == 0:
-                total.append(n)
-            else:
-                total.append(total[i-1] + n)
-        return total
