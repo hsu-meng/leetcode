64bc0c4ba21448f4552dc432e5c3e6286c5e51f9
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 14:08:17 +0800

    Add files via upload

--- Array/1512_NumberofGoodPairs/goodPairs.py
+++ Array/1512_NumberofGoodPairs/goodPairs.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        pairs = 0
+        for i, n in enumerate(nums):
+            for j in range(i+1, len(nums)):
+                if n == nums[j]:
+                    pairs += 1
+        return pairs
