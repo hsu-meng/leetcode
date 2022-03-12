da69f24c13aa619772a08315814c12abec895655
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 16:55:27 +0800

    Add files via upload

--- Array/41_FirstMissingPositive/missing.py
+++ Array/41_FirstMissingPositive/missing.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = [n for n in nums if n > 0] # Remove the negative numbers from the list
+        nums.sort()
+        nums = list(dict.fromkeys(nums)) # Remove any duplicates from the list
+        for i in range(len(nums)):
+            if i+1 != nums[i]:
+                return i+1
+        return len(nums) + 1
