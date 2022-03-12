bcbc999c62dda9cba38e8111f89c4b625ddbdc5e
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 14:49:49 +0800

    Add files via upload

---
+++ Math/1281_SubtracttheProductandSumofDigitsofanInteger/multi-add_.py
@@ -0,0 +1,9 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        digits = [int(d) for d in str(n)]
+        multi = 1
+        add = 0
+        for d in digits:
+            multi *= d
+            add += d
+        return multi - add
