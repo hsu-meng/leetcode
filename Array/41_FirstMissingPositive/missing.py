bbcbead8100d1bfaf77c03d66835d6c8c5914eb1
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 16:57:51 +0800

    Update missing.py

--- Array/41_FirstMissingPositive/missing.py
+++ Array/41_FirstMissingPositive/missing.py
@@ -3,7 +3,7 @@ class Solution:
         nums = [n for n in nums if n > 0] # Remove the negative numbers from the list
         nums.sort()
         nums = list(dict.fromkeys(nums)) # Remove any duplicates from the list
-        for i in range(len(nums)):
+        for i in range(len(nums)): # O(n)
             if i+1 != nums[i]:
                 return i+1
         return len(nums) + 1
