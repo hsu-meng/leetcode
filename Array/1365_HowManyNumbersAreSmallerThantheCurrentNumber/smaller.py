44f7173582b8c8eec3f19a091f804de2369f9b2f
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 14:38:38 +0800

    Add files via upload

--- Array/1365_HowManyNumbersAreSmallerThantheCurrentNumber/smaller.py
+++ Array/1365_HowManyNumbersAreSmallerThantheCurrentNumber/smaller.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        ans = []
+        for n in nums:
+            count = 0
+            for m in nums:
+                if n > m:
+                    count += 1
+            ans += [count]
+        return ans
