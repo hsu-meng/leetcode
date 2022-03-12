11cf9d5b06412fcdd2e88d33bc94be607af71a00
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 15:51:18 +0800

    Add files via upload

--- Array/804_UniqueMorseCodeWords/morse.py
+++ Array/804_UniqueMorseCodeWords/morse.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morseList = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        index = 0
+        for i in words:
+            replace = ""
+            for j in i:
+                replace += morseList[ord(j) - 97]
+            words[index] = replace
+            index += 1
+        
+        words = set(words)
+        return len(words)
