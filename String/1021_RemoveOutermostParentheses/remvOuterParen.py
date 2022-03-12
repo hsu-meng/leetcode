e768be7ed777af1c2e86eeaffe3be8516b1613c8
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 15:21:09 +0800

    Add files via upload

--- String/1021_RemoveOutermostParentheses/remvOuterParen.py
+++ String/1021_RemoveOutermostParentheses/remvOuterParen.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        stack = []
+        output = ''
+        for i in s:
+            if i == '(':
+                if stack:
+                    output += i
+                stack.append(i)
+            elif i == ')':
+                if stack[-1] == '(':
+                    stack.pop()
+                else:
+                    stack.append(i)
+                if stack:
+                    output += i
+        return output
