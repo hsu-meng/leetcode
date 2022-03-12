63af3240bdc15f1c55d440982bebc5800924944a
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 14:54:41 +0800

    Add files via upload

--- Array/1313_DecompressRun-LengthEncodedList/run-lengthList.py
+++ Array/1313_DecompressRun-LengthEncodedList/run-lengthList.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i, n in enumerate(nums):
+            if i % 2 == 0:
+                freq = n
+            else:
+                for f in range(freq):
+                    ans.append(n)
+        return ans
