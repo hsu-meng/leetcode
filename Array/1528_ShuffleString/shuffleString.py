8826be68a3d9d895a6eb7d63440df1df1662abbe
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 14:43:38 +0800

    Add files via upload

--- Array/1528_ShuffleString/shuffleString.py
+++ Array/1528_ShuffleString/shuffleString.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        ans = ""
+        for i in range(len(indices)):
+            ans += s[indices.index(i)]
+        return ans
