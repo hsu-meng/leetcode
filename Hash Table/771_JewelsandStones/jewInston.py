9290962548a4856677299a358c125c445487b4ba
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 14:14:31 +0800

    Add files via upload

--- Hash Table/771_JewelsandStones/jewInston.py
+++ Hash Table/771_JewelsandStones/jewInston.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for s in stones:
+            if jewels.find(s) != -1:
+                count += 1
+        return count
