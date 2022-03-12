cce8c6b7b09b2c10ee47e45ac23654aa013ea32b
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 15:04:17 +0800

    Add files via upload

--- String/1221_SplitaStringinBalancedStrings/balancedStr.py
+++ String/1221_SplitaStringinBalancedStrings/balancedStr.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        count = 0
+        start = s[0]
+        pairs = 0
+        for i in s:
+            if start == i:
+                pairs += 1
+            else:
+                pairs -= 1
+            if pairs == 0:
+                count += 1
+        return count
