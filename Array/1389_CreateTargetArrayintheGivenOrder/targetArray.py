f5bc17e03c1a5eebfde4031187a9852f5452e67e
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 14:57:49 +0800

    Add files via upload

--- Array/1389_CreateTargetArrayintheGivenOrder/targetArray.py
+++ Array/1389_CreateTargetArrayintheGivenOrder/targetArray.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        output = []
+        for i in range(len(index)):
+            output.insert(index[i], nums[i])
+        return output
