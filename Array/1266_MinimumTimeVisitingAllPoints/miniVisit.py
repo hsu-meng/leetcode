39dfcb7150bb8e23b3e79a1f53b911f414dc4c60
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 15:17:35 +0800

    Add files via upload

--- Array/1266_MinimumTimeVisitingAllPoints/miniVisit.py
+++ Array/1266_MinimumTimeVisitingAllPoints/miniVisit.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        output = 0
+        for i in range(len(points) - 1):
+            x = abs(points[i][0] - points[i+1][0])
+            y = abs(points[i][1] - points[i+1][1])
+            output += max(x, y)
+        return output
