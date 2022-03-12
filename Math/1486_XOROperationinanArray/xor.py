7fee185aee8ca178585e7b434b878b8f9c6d945e
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-12 15:00:52 +0800

    Add files via upload

--- Math/1486_XOROperationinanArray/xor.py
+++ Math/1486_XOROperationinanArray/xor.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        output = 0
+        nums = [0] * n
+        for i in range(n):
+            nums[i] = start + 2 * i
+            output ^= nums[i]
+        return output
