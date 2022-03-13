780dbdbf3d4eecc75856023d3898cb73abe91cd9
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-13 14:00:43 +0800

    Add files via upload

--- Array/1014_BestSightseeingPair/bestPair.py
+++ Array/1014_BestSightseeingPair/bestPair.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        output = 0
+        pre = 0
+        for i in range(1, len(values)): # O(N),  find maximum of (values[i] + i) and (values[j] - j)
+            output = max(values[pre] + values[i] + pre - i, output)
+            if values[i] + i > values[pre] + pre:
+                pre = i
+        return output
