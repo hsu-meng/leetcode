7011e770716f3e5fb949c0484026648311ae5f62
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-13 12:51:23 +0800

    Add files via upload

--- Math/1323_Maximum69Number/max69.py
+++ Math/1323_Maximum69Number/max69.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        strNum = str(num)
+        output = ''
+        once = 0
+        for s in strNum:
+            if s == '6' and once == 0:
+                s = '9'
+                once = 1
+            output += s
+        return output
