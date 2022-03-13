df9db0f9ecd5a705c402859d295113ba798adb5c
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-13 12:28:42 +0800

    Add files via upload

--- Array/1252_CellswithOddValuesinaMatrix/oddCells.py
+++ Array/1252_CellswithOddValuesinaMatrix/oddCells.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
+        matrix = [[0]*n for _ in range(m)] # Initial matrix
+        count = 0
+        for i in indices:
+            r = i[0]
+            c = i[1]
+            for m, col in enumerate(matrix):
+                for n, row in enumerate(col):
+                    if m == r:
+                        matrix[m][n] += 1
+                    if n == c:
+                        matrix[m][n] += 1
+        for lines in matrix:
+            for elements in lines:
+                if elements%2 == 1:
+                    count += 1
+        return count
