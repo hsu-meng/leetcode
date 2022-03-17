b8aa90b11395ff9b52a0581906344e6a3d592424
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-17 20:42:58 +0800

    Add files via upload

--- Array/697_DegreeofanArray/degreeArr.py
+++ Array/697_DegreeofanArray/degreeArr.py
@@ -1,1 +1,20 @@
-
+from collections import Counter
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        c_nums = Counter(nums)
+        degree = max(list(c_nums.values()))
+        last = len(nums)
+        left = {}
+        right = {}
+        count = {}
+        for i, n in enumerate(nums):
+            if n not in left:
+                left[n] = i
+                count[n] = 0
+            count[n] += 1
+            right[n] = i
+        for n, c in count.items():
+            if c == degree:
+                last = min(last, right[n] - left[n] + 1)
+        return last
+                    
