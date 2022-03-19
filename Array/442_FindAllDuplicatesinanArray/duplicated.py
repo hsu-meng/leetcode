3619cfaf8522f8c81d06ac86192721c5a686142b
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-19 15:49:44 +0800

    Add files via upload

--- Array/442_FindAllDuplicatesinanArray/duplicated.py
+++ Array/442_FindAllDuplicatesinanArray/duplicated.py
@@ -1,1 +1,9 @@
-
+from collections import Counter
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        init = Counter(nums)
+        output = []
+        for k, v in init.items():
+            if v == 2:
+                output.append(k)
+        return output
