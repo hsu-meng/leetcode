94acfd8152c70ff2412fc0cd9f4f0a9c2237e664
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-19 17:07:48 +0800

    Add files via upload

--- Array/274_H-Index/H-Index.py
+++ Array/274_H-Index/H-Index.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        citations.sort()
+        for i, c in enumerate(citations):
+            if n - i <= c:
+                return n - i # h-index = min(n-i, citations[i])
+        return 0
