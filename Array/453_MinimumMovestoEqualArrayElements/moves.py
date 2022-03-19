ee7dcfa3a2b24028644f5b9ea620a33cbfd94317
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-19 19:41:39 +0800

    Add files via upload

--- Array/453_MinimumMovestoEqualArrayElements/moves.py
+++ Array/453_MinimumMovestoEqualArrayElements/moves.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def minMoves(self, nums: List[int]) -> int:
+        # sum + (n - 1) * moves = equal_value * n
+        # min_element + moves = equal_value
+        # => moves = sum - n * min_element
+        return sum(nums) - len(nums) * min(nums)
