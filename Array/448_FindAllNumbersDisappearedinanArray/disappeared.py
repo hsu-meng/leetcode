770730be570f924a5510b3da232fa8b1247c28a9
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-19 15:19:03 +0800

    Add files via upload

--- Array/448_FindAllNumbersDisappearedinanArray/disappeared.py
+++ Array/448_FindAllNumbersDisappearedinanArray/disappeared.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        output = []
+        uni = set(nums)
+        for i in range(1, len(nums)+1):
+            if i not in uni:
+                output.append(i)
+        return output
