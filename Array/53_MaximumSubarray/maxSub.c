9b64852cc77fd6a1407a10240872d441880832c0
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-25 12:46:06 +0800

    Add files via upload

--- Array/53_MaximumSubarray/maxSub.c
+++ Array/53_MaximumSubarray/maxSub.c
@@ -1,1 +1,11 @@
+#define MAX(a, b) (((a)>(b))?(a):(b))
 
+int maxSubArray(int* nums, int numsSize){
+    int i = 0, j = 0;
+    int max = nums[0];
+    for(i = 1; i < numsSize; i++){
+        nums[i] = MAX(nums[i], nums[i] + nums[i-1]);
+        max = MAX(max, nums[i]);
+    }
+    return max;
+}
