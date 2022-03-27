69a6b42ca85c7278848e388a43916c581d533d6b
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-27 10:32:14 +0800

    Update maxSub.c

--- Array/53_MaximumSubarray/maxSub.c
+++ Array/53_MaximumSubarray/maxSub.c
@@ -1,7 +1,7 @@
-#define MAX(a, b) (((a)>(b))?(a):(b))
+#define MAX(a, b) ((a>b)?a:b)
 
 int maxSubArray(int* nums, int numsSize){
-    int i = 0, j = 0;
+    int i = 0;
     int max = nums[0];
     for(i = 1; i < numsSize; i++){
         nums[i] = MAX(nums[i], nums[i] + nums[i-1]);
