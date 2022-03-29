ffdac85bf7a1f446572a75ebf6068bc88ae48ef3
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-29 16:08:31 +0800

    Update binSearch.c

--- Array/704_BinarySearch/binSearch.c
+++ Array/704_BinarySearch/binSearch.c
@@ -2,17 +2,23 @@ int search(int* nums, int numsSize, int target){
     int left = 0;
     int right = numsSize - 1;
     int mid;
+    // while left <= right, indicates that there's element in the interval
     while(left <= right){
+        // update the middle value
         mid = (left + right) / 2;
+        // the target is located in [left, middle-1] interval
         if(nums[mid] > target){
             right = mid - 1;
         }
+        // the target is located in [middle+1, right] interval
         else if(nums[mid] < target){
             left = mid + 1;
         }
+        // the target is equal to the middle value
         else{
             return mid;
         }
     }
+    // if not found the target element, return -1
     return -1;
 }
