5039ea8014472125c93bf381154756c3c9e82fb4
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-03-29 16:01:21 +0800

    Add files via upload

--- Array/704_BinarySearch/binSearch.c
+++ Array/704_BinarySearch/binSearch.c
@@ -1,1 +1,18 @@
-
+int search(int* nums, int numsSize, int target){
+    int left = 0;
+    int right = numsSize - 1;
+    int mid;
+    while(left <= right){
+        mid = (left + right) / 2;
+        if(nums[mid] > target){
+            right = mid - 1;
+        }
+        else if(nums[mid] < target){
+            left = mid + 1;
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+}
