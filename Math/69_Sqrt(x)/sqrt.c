af410b09f5caf307e5894a9b5eef2a192f8b97ab
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-04-01 12:15:33 +0800

    Add files via upload

--- Math/69_Sqrt(x)/sqrt.c
+++ Math/69_Sqrt(x)/sqrt.c
@@ -1,1 +1,13 @@
-
+// Brute Force solution
+// Time complexity: sqrt(x)
+int mySqrt(int x){
+    if(x <= 1){
+        return x;
+    }
+    for(long long i = 1; i <= x; i++){
+        if(i * i > x){
+            return i - 1;
+        }
+    }
+    return 1;
+}
