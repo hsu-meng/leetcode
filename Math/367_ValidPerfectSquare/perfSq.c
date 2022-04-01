9673db18665c7177fbaa23f0538bcab0f8d4a4e4
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-04-01 12:49:21 +0800

    Add files via upload

---
+++ Math/367_ValidPerfectSquare/perfSq.c
@@ -0,0 +1,17 @@
+bool isPerfectSquare(int num){
+    long left = 0;
+    long right = num;
+    while(left <= right){
+        long mid = (left + right) / 2;
+        if(mid * mid == num){
+            return true;
+        }
+        else if(mid * mid > num){
+            right = mid - 1;
+        }
+        else{
+            left = mid + 1;
+        }
+    }
+    return false;
+}
