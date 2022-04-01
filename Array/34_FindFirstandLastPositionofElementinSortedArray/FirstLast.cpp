6d41b6edb38031f61a2de7f68d5bdbacea585157
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-04-01 11:11:14 +0800

    Add files via upload

--- Array/34_FindFirstandLastPositionofElementinSortedArray/FirstLast.cpp
+++ Array/34_FindFirstandLastPositionofElementinSortedArray/FirstLast.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int leftBorder = search(nums, target);
+        int rightBorder = search(nums, target + 1) - 1;
+        if(rightBorder - leftBorder >= 0){
+            return {leftBorder, rightBorder};
+        }
+        else{
+            return {-1, -1};
+        }
+    }
+private:
+    int search(vector<int>& nums, int target);
+};
 
+int Solution::search(vector<int>& nums, int target){
+    int left = 0;
+    int right = nums.size() - 1;
+    while(left <= right){
+        int mid = (left + right) / 2;
+        if(target > nums[mid]){
+            left = mid + 1;
+        }
+        else{
+            right = mid - 1;
+        }
+    }
+    return left;
+}
