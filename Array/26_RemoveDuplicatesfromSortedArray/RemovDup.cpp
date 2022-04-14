c0c1fc9a4cf805c3d6df235614ce68feb967e386
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-04-14 20:48:27 +0800

    Update RemovDup.cpp

--- Array/26_RemoveDuplicatesfromSortedArray/RemovDup.cpp
+++ Array/26_RemoveDuplicatesfromSortedArray/RemovDup.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        // use the two-pointer technique to move the least number of elements
         int slow = 0;
         int fast;
         for(fast = 1; fast < nums.size(); ++fast){
