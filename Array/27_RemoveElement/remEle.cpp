7f3c7b42641a958a80b89525e650361f989ca4a9
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-04-14 20:15:42 +0800

    Add files via upload

--- Array/27_RemoveElement/remEle.cpp
+++ Array/27_RemoveElement/remEle.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slow = 0;
+        int fast;
+        for(fast = 0; fast < nums.size(); ++fast){
+            if(nums[fast] != val){
+                nums[slow++] = nums[fast];
+            }
+        }
+        return slow;
+    }
+};
