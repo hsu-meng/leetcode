8d93c3d4ddac0699d258045dab0362d8405e9948
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-04-14 20:22:33 +0800

    Update RemovEle.cpp

--- Array/27_RemoveElement/RemovEle.cpp
+++ Array/27_RemoveElement/RemovEle.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        // use the two-pointer technique to move the least number of elements
         int slow = 0;
         int fast;
         for(fast = 0; fast < nums.size(); ++fast){
