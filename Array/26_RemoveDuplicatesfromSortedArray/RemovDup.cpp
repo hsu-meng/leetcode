815e3d0b54138a9c6318c3ec9b2f8481a9b984e0
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-04-14 20:47:30 +0800

    Add files via upload

--- Array/26_RemoveDuplicatesfromSortedArray/RemovDup.cpp
+++ Array/26_RemoveDuplicatesfromSortedArray/RemovDup.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int slow = 0;
+        int fast;
+        for(fast = 1; fast < nums.size(); ++fast){
+            if(nums[slow] != nums[fast]){
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+        return slow + 1;
+    }
+};
