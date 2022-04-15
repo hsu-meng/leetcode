063658f3470b70d0fefe4bf687b2d7d9027562e8
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-04-15 16:12:44 +0800

    Add files via upload

--- Array/283_MoveZeroes/MovZero.cpp
+++ Array/283_MoveZeroes/MovZero.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int slow = 0;
+        int fast;
+        int i;
+        for(fast = 0; fast < nums.size(); ++fast){
+            if(nums[fast] != 0){
+                nums[slow++] = nums[fast];
+            }
+        }
+        for(i = slow; i < nums.size(); ++i){
+            nums[i] = 0;
+        }
+    }
+};
