33fa6fd27e1604a08393549bb0acd6d4483dee20
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-04-15 16:22:18 +0800

    Update MovZero.cpp

--- Array/283_MoveZeroes/MovZero.cpp
+++ Array/283_MoveZeroes/MovZero.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        // use the two-pointer technique to move the least number of elements
         int slow = 0;
         int fast;
         int i;
@@ -9,6 +10,7 @@ public:
                 nums[slow++] = nums[fast];
             }
         }
+        // fill with 0 from index of slow to size
         for(i = slow; i < nums.size(); ++i){
             nums[i] = 0;
         }
