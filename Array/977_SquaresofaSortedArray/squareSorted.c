76df20b5910b2f398bdb20b70304b7b0d1f49dc4
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-05-04 19:03:48 +0800

    Add files via upload

--- Array/977_SquaresofaSortedArray/squareSorted.c
+++ Array/977_SquaresofaSortedArray/squareSorted.c
@@ -1,1 +1,20 @@
-
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* sortedSquares(int* nums, int numsSize, int* returnSize){
+    int* out = malloc(sizeof(int)* numsSize);
+    *returnSize = numsSize;
+    int start = 0;
+    int end = numsSize - 1;
+    for(int i = numsSize - 1; i >= 0; i--){
+        if(abs(nums[start]) > abs(nums[end])){
+            out[i] = pow(nums[start], 2);
+            start++;
+        }
+        else{
+            out[i] = pow(nums[end], 2);
+            end--;
+        }
+    }
+    return out;
+}
