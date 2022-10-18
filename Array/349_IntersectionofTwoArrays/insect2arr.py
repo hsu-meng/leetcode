d041d9439bcc3449ddf43eda3d3150c33c2522ae
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2022-10-18 10:40:08 +0800

    Update insect2arr.py

--- Array/349_IntersectionofTwoArrays/insect2arr.py
+++ Array/349_IntersectionofTwoArrays/insect2arr.py
@@ -1,3 +1,3 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list(set(nums1) & set(nums2))
+        return list(set(nums1) & set(nums2)) # find the intersection of two array and turn (set) back to list
