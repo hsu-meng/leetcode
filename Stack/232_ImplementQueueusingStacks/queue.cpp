1e5453a01eb95d4ba47c211790635081a85d227e
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2023-04-27 17:51:46 +0800

    Create queue.cpp

---
+++ Stack/232_ImplementQueueusingStacks/queue.cpp
@@ -0,0 +1,42 @@
+class MyQueue {
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop() {
+        int out = peek();
+        s2.pop();
+        return out;
+    }
+    
+    int peek() {
+        // push upside-down
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
+    }
+    
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+private:
+    stack<int> s1, s2;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
