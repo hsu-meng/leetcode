a143fb24f3ef678b039c514136dcdc15fa766497
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2023-09-12 17:37:26 +0800

    Create nextPermu.cpp

---
+++ Array/31_NextPermutation/nextPermu.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return;
+      
+        int pivot = n - 2;
+        while(pivot >= 0 && nums[pivot] >= nums[pivot + 1]) pivot--; // Searching backward and find the first position that breaks the monotonic increasing property
+        if(pivot < 0) { // Array is in a monotonically decreasing order
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        
+        int idx = pivot + 1;
+        while(idx < n && nums[idx] > nums[pivot]) idx++;
+        swap(nums[idx - 1], nums[pivot]); // Swap nums[idx] with the smallest number greater than nums[pivot] from nums[pivot + 1, n) (if there are multiple, choose the one furthest to the right)
+        reverse(nums.begin() + pivot + 1, nums.end()); // Then, reverse the nums[pivot + 1, n)
+    }
+};
