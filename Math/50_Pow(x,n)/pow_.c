02f245f16496fdc6ecf84bc8a6c9e17160fa6a2c
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2023-10-09 20:09:37 +0800

    Create pow_.c

---
+++ Math/50_Pow(x,n)/pow_.c
@@ -0,0 +1,9 @@
+double myPow(double x, int n){
+    double ans = 1;
+    while(n) {
+        if(n % 2) ans = n > 0 ? ans * x : ans / x;
+        x = x * x;
+        n /= 2;
+    }
+    return ans;
+}
