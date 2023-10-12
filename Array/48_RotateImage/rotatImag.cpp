5ef3c6b1e368ad99dfe0c878b2dc0d9b191f83b0
hsu-meng <72554238+hsu-meng@users.noreply.github.com>
2023-10-12 17:45:19 +0800

    Update rotatImag.cpp

--- Array/48_RotateImage/rotatImag.cpp
+++ Array/48_RotateImage/rotatImag.cpp
@@ -4,8 +4,8 @@ public:
         int n = matrix.size();
         for(int i = 0; i < n; i++)
             for(int j = 0; j <= i; j++)
-                swap(matrix[i][j], matrix[j][i]);
+                swap(matrix[i][j], matrix[j][i]); //convert row to column
         for(int i = 0; i < n; i++)
-            reverse(matrix[i].begin(), matrix[i].end());
+            reverse(matrix[i].begin(), matrix[i].end()); //reverse each row
     }
 };
